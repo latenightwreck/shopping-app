88027d279df8aea14ef4ec488b1aeb0a6087b1a1
Morgan Blaze <morgan.b.armstrong@gmail.com>
2018-10-22 00:01:49 -0400

    Adding HTML for recipes list, as well as details of recipe

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RepcipeListComponent implements OnInit {
+
+  recipes: Recipe[] = [
+    new Recipe('Banana Bread', 'This is some banana bread.',
+    'https://food.fnr.sndimg.com/content/dam/images/food/fullset/2010/4/13/0/GC_banana-bread_s4x3.jpg.rend.hgtvcom.826.620.suffix/1371592847747.jpeg')
+  ];
 
   constructor() { }
 
