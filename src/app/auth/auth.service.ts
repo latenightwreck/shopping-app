1246c341b1c5ef1b2108c6ad73edc1eeb96d0739
latenightwreck <morgan.b.armstrong@gmail.com>
2020-10-10 23:59:24 -0400

    Remove Key

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -23,6 +23,7 @@ export class AuthService {
   signup(email: string, password: string) {
     return this.http
       .post<AuthResponseData>(
+
         'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=[KEY]',
         { email: email, password: password, returnSecureToken: true }
       )
